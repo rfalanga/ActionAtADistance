89d5300a6a47cfc5877aa34ef3bf84da02ffbfd5
Rod Falanga <rfalanga@users.noreply.github.com>
2020-02-15 06:59:13 -0700

    fleshed out MysteryBooms VM

--- ActionAtaDistance1/ActionAtaDistance1/ViewModel/AuthorsViewModel.cs
+++ ActionAtaDistance1/ActionAtaDistance1/ViewModel/AuthorsViewModel.cs
@@ -1,10 +1,7 @@
 ﻿using GalaSoft.MvvmLight;
 using ActionAtaDistance1.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ActionAtaDistance1.ViewModel
 {
--- ActionAtaDistance1/ActionAtaDistance1/ViewModel/MysteryBooksViewModel.cs
+++ ActionAtaDistance1/ActionAtaDistance1/ViewModel/MysteryBooksViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using ActionAtaDistance1.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,28 @@ namespace ActionAtaDistance1.ViewModel
 {
     public class MysteryBooksViewModel : ViewModelBase
     {
+        public List<MysteryBook> MysteryBooks { get; set; }
+
+        private MysteryBook selectedMysteryBook;
+        public MysteryBook SelectedMysteryBook 
+        { 
+            get { return selectedMysteryBook; }
+            set
+            {
+                if (selectedMysteryBook != value)
+                {
+                    selectedMysteryBook = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public MysteryBooksViewModel()
+        {
+            using (var ctx = new AuthorsModel())
+            {
+                MysteryBooks = ctx.MysteryBooks.OrderBy(m => m.BookTitle).ToList();
+            }
+        }
     }
 }
