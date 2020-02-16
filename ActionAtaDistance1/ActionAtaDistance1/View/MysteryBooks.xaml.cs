8b1077932231a4e6f5a7c6297948dd139f296b1d
Rod Falanga <rfalanga@users.noreply.github.com>
2020-02-15 17:39:24 -0700

    added simple Click event to the Cancel button, to close the window

--- ActionAtaDistance1/ActionAtaDistance1/View/MysteryBooks.xaml.cs
+++ ActionAtaDistance1/ActionAtaDistance1/View/MysteryBooks.xaml.cs
@@ -23,5 +23,10 @@ namespace ActionAtaDistance1.View
         {
             InitializeComponent();
         }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
